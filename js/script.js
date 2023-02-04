5e15572f44e5877a1437ee5d1a039e2b73900f53
Daniar02 <abykeev02@icloud.com>
2023-02-04 19:30:07 +0600

    Работа с датами

--- js/script.js
+++ js/script.js
@@ -50,41 +50,68 @@
 // wrapper.append(btn);
 
 /*######################## Скрипты и время их выполнения. setTimeout и setInterval ######################## */
-const btn = document.querySelector('.btn');
-let timerId,
-    i = 0;
+// const btn = document.querySelector('.btn');
+// let timerId,
+//     i = 0;
 
-function myAnimation() {
-    const elem = document.querySelector('.box');
-    let pos = 0;
+// function myAnimation() {
+//     const elem = document.querySelector('.box');
+//     let pos = 0;
 
-    const id = setInterval(frame, 10);
-    function frame() {
-        if (pos == 300) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.top = pos + "px";
-            elem.style.left = pos + "px";
-        }
-    }
-}
-
-btn.addEventListener('click', myAnimation);
-
-// const timerId = setTimeout(function(text) {
-//     console.log(text);
-// }, 2000, 'Hello');
-
-// function logger() {
-//     if (i === 3) {
-//         clearInterval(timerId);
+//     const id = setInterval(frame, 10);
+//     function frame() {
+//         if (pos == 300) {
+//             clearInterval(id);
+//         } else {
+//             pos++;
+//             elem.style.top = pos + "px";
+//             elem.style.left = pos + "px";
+//         }
 //     }
-//     console.log('text');
-//     i++;
 // }
 
-// var id = setTimeout(function log() {
-//     console.log('Hello');
-//     id = setTimeout(log, 500);
-// }, 500);
+// btn.addEventListener('click', myAnimation);
+
+// // const timerId = setTimeout(function(text) {
+// //     console.log(text);
+// // }, 2000, 'Hello');
+
+// // function logger() {
+// //     if (i === 3) {
+// //         clearInterval(timerId);
+// //     }
+// //     console.log('text');
+// //     i++;
+// // }
+
+// // var id = setTimeout(function log() {
+// //     console.log('Hello');
+// //     id = setTimeout(log, 500);
+// // }, 500);
+
+/*################################### Работа с датами ################################### */
+
+const now = new Date();
+
+console.log(now.setHours(19));
+console.log(now);
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`Цикл отработал за ${end - start} миллисекунд`);
