f63a1a172bb9df117ba295b440470b3ef315270e
Daniar02 <abykeev02@icloud.com>
2023-02-05 22:18:12 +0600

    Классы (ES6)

--- js/script.js
+++ js/script.js
@@ -144,27 +144,141 @@
 // console.log(document.documentElement.scrollTop);
 
 /*################################### Функции-конструкторы ################################### */
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.hello = function () {
-        console.log(`hello ${this.name}`);
-    };
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function () {
+//         console.log(`hello ${this.name}`);
+//     };
+// }
+
+// User.prototype.exit = function() {
+//     console.log(`Пользователь ${this.name} ушел`);
+// };
+
+// const ivan = new User('Ivan', 28);
+// const alex = new User('Alex', 20);
+
+// ivan.hello();
+// alex.hello();
+
+// ivan.exit();
+// alex.exit();
+
+// console.log(ivan);
+// console.log(alex);
+
+/*################################### Контекст вызова. This ################################### */
+
+// function showThis(a, b) {
+//     console.log(this);
+
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
+
+//     console.log(sum());
+// }
+// showThis(4, 5);
+
+// const obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         console.log(this);
+//     }
+// };
+
+// obj.sum();
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+// }
+
+// let ivan = new User('Ivan', 23);
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+
+// const user = {
+//     name: 'john'
+// };
+
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
+
+// function count(num) {
+//     return this*num;
+// }
+
+// const double = count.bind(2);
+// console.log(double(3));
+// console.log(double(13));
+
+// 1) Обычная функция: this = window, но если use strict - undefined
+// 2) Контекст у методов объекта - сам объект
+// 3) this в конструкторах и классах - это новый экземпляр объекта
+// 4) Ручная привязка this: call, apply, bind
+
+// const btn = document.querySelector('button');
+
+// btn.addEventListener('click', (e) => {
+//     e.target.style.backgroundColor = 'red';
+// });
+
+// const obj = {
+//     num: 5,
+//     sayNumber: function() {
+//         const say = () => {
+//             console.log(this.num);
+//         }; 
+//         say();
+//     }
+// };
+// obj.sayNumber();
+
+// const double = (a) => {
+//     return a * 2;
+// };
+// console.log(double(2));
+
+/*################################### Классы (ES6) ################################### */
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
 }
 
-User.prototype.exit = function() {
-    console.log(`Пользователь ${this.name} ушел`);
-};
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
 
-const ivan = new User('Ivan', 28);
-const alex = new User('Alex', 20);
+    showMyProps() {
+        console.log(`Текст: ${this.text}, Цвет: ${this.bgColor}`);
+    }
+}
 
-ivan.hello();
-alex.hello();
+const div = new ColoredRectangleWithText(25, 10, 'Hello World', 'red');
 
-ivan.exit();
-alex.exit();
+div.showMyProps();
+console.log(div.calcArea);
 
-console.log(ivan);
-console.log(alex);
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(20, 100);
+
+// console.log(long.calcArea());
+// console.log(square.calcArea());
