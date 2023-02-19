08772931cef7a37584a8b6992d3af696bf7f6290
Daniar02 <abykeev02@icloud.com>
2023-02-19 23:17:36 +0600

    Find the treasure.

--- js/game.js
+++ js/game.js
@@ -60,3 +60,71 @@ const generateRandomInsult = function () {
 console.log(generateRandomInsult());
 console.log(generateRandomInsult());
 console.log(generateRandomInsult());
+
+// Пишем игру. Найди клад.
+// Получить случайное число от 0 до size-1
+let getRandomNumber = function(size) {
+    return Math.floor(Math.random() * size);
+};
+
+// Вычислить расстояние от клика (event) до клада (target)
+let getDistance = function (event, target) {
+    let diffX = event.offsetX - target.x;
+    let diffY = event.offsetY - target.y;
+    return Math.sqrt((diffX * diffX) + (diffY * diffY));
+};
+
+// Получить для расстояния строку подсказки
+let getDistanceHint = function (distance) {
+    if (distance < 10) {
+        return "Обожжешься!";
+
+    } else if (distance < 20) {
+        return "Очень горячо";
+
+    } else if (distance < 40) {
+        return "Горячо";
+
+    } else if (distance < 80) {
+        return "Тепло";
+
+    } else if (distance < 160) {
+        return "Холодно";
+
+    } else if (distance < 320) {
+        return "Очень Холодно";
+
+    } else {
+        return "Замерзнешь";
+
+    }
+};
+
+// Создаем переменные
+let width = 400;
+let height = 400;
+let clicks = 0;
+
+// Случайная позиция клада
+let target = {
+    x: getRandomNumber(width),
+    y: getRandomNumber(height)
+};
+
+// Добавляем элементу img обработчик клика
+$("#map").click(function (event) {
+    clicks++;
+    // Получаем расстояние от места клика до клада
+    let distance = getDistance(event, target);
+
+// Преобразуем расстояние в подсказку
+    const distanceHint = getDistanceHint(distance);
+
+// Записываем в элемент #distance Новую подсказку
+    $("#distance").text(distanceHint);
+
+// Если клик был достаточно близко, поздравляем с победой
+    if (distance < 8) {
+        alert("Клад найден! сделано кликов: " + clicks);
+    }
+});
